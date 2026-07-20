a138ec187af7fe7f3e4278d4a5b9f6b4f3b87884
Phone Win Tun <phonewinttun173@gmail.com>
2026-07-21 01:05:28 +0630

    feat: registered newly added features through dependency injection

--- ACST.Domain/Features/FeatureManager.cs
+++ ACST.Domain/Features/FeatureManager.cs
@@ -1,12 +1,14 @@
 using ACST.Database.ApplicationDbContextModels.Models;
 using ACST.Domain.Features.Analytics;
+using ACST.Domain.Features.Auth;
 using ACST.Domain.Features.ClassSessions;
 using ACST.Domain.Features.GoogleCalendar;
 using ACST.Domain.Features.Holidays;
 using ACST.Domain.Features.Modules;
 using ACST.Domain.Features.RecurringSchedules;
-using ACST.Domain.Features.Semesters;
+using ACST.Domain.Features.RolePermission;
 using ACST.Domain.Features.Search;
+using ACST.Domain.Features.Semesters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +25,8 @@ namespace ACST.Domain.Features
                 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             // Feature Services
+            builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddScoped<IRolePermissionService, RolePermissionService>();
             builder.Services.AddScoped<IModuleService, ModuleService>();
             builder.Services.AddScoped<ISemesterService, SemesterService>();
             builder.Services.AddScoped<IRecurringScheduleService, RecurringScheduleService>();
